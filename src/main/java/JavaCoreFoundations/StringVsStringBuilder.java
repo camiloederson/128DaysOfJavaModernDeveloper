8be8ee8e4e665121e5abf18fa644370ca473edd1
MikaDev <camilo.ederson@gmail.com>
2025-11-04 16:50:40 -0600

    feat: add a String vs Stringbuilder comparer

---
+++ src/main/java/JavaCoreFoundations/StringVsStringBuilder.java
@@ -0,0 +1,18 @@
+package JavaCoreFoundations;
+
+public class StringVsStringBuilder {
+    public static void main(String[] args){
+        compareStrings();
+    }
+
+    public static void compareStrings(){
+        long start = System.currentTimeMillis();
+        String cadenaString = "";
+        for (int i = 0; i < 10000; i++) {
+            cadenaString += "palabra" + i + " ";
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(cadenaString);
+        System.out.println(end - start);
+    }
+}
