e4d2b5fe9378b921bf87c70a3778f5727053d012
MikaDev <camilo.ederson@gmail.com>
2025-11-04 17:48:14 -0600

    feat: demonstrating how a String and a StringBuilder work

---
+++ src/main/java/JavaCoreFoundations/MemoryDemo.java
@@ -0,0 +1,37 @@
+package JavaCoreFoundations;
+
+public class MemoryDemo {
+    public static void main(String[] args){
+
+        String string1 = "String";
+        String string2 = "String";
+
+        String string3 = new String("NewString");
+        String string4 = new String("NewString");
+
+        Person person1 = new Person("Camilo", 38);
+        Person person2 = new Person("Camilo", 38);
+
+        StringBuilder stringBuilder = new StringBuilder("sb");
+
+        // Comparing == and equals
+        System.out.println(string1 == string2); // should be true because of the Spring Pool
+        System.out.println(string3 == string4); // should be false
+
+        System.out.println(string1.equals(string2)); // should be true
+        System.out.println(string3.equals(string4)); // should be true
+
+        System.out.println(string1.hashCode());
+        string1 += " concatenated";
+        System.out.println(string1.hashCode());
+
+        // Demonstrating StringBuilder mutability
+        System.out.println(string3.hashCode()); // hashCode of a SB string
+        string3.concat(" concatenated"); // should print the same hash
+        System.out.println(string3.hashCode());
+
+
+
+
+    }
+}
---
+++ src/main/java/JavaCoreFoundations/Person.java
@@ -0,0 +1,34 @@
+package JavaCoreFoundations;
+
+import java.util.Objects;
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Overriding the toString method.
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
+    }
+
+    // Overriding the equals method to make sure that we compare content and not references.
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return this.age == person.age && this.name.equals(person.name);
+    }
+
+    // Overriding the hash method to make sure that the integer is going to be the one we need
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
