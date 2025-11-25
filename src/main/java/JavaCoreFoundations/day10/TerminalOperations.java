e7d8f9fd72278cd2741c16e929ed83e52daae883
MikaDev <camilo.ederson@gmail.com>
2025-11-25 14:45:11 -0600

    feat: Stream Terminal Operations: count

--- src/main/java/JavaCoreFoundations/day10/TerminalOperations.java
+++ src/main/java/JavaCoreFoundations/day10/TerminalOperations.java
@@ -15,12 +15,25 @@ public class TerminalOperations {
             return integers;
         };
 
-        printingList(listOfIntegers);
+        Supplier<List<Integer>> list = () -> {
+            int index = (int) (Math.random() * 100 + 1);
+            List<Integer> integers = new ArrayList<>();
+            for (int i = 0; i < index; i++) {
+                integers.add((int) (Math.random() * 100 + 1));
+            }
+            return integers;
+        };
 
+        printingList(listOfIntegers);
+        System.out.println("🖥️ The second list contains: " + countingList(list) + " elements");
         }
 
         public static void printingList(Supplier<List<Integer>> listSupplier){
             List<Integer> list = listSupplier.get();
             list.stream().sorted().forEach(System.out::println);
+        }
+
+        public static long countingList(Supplier<List<Integer>> list){
+            return  list.get().stream().count();
         }
     }
