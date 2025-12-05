4f091bb790fdfe5dfc8be128b2280620fa879391
MikaDev <camilo.ederson@gmail.com>
2025-12-05 16:04:03 -0600

    feat: Enums with constructors

--- src/main/java/JavaCoreFoundations/day12/EnumExample.java
+++ src/main/java/JavaCoreFoundations/day12/EnumExample.java
@@ -1,7 +1,17 @@
 package JavaCoreFoundations.day12;
 
 public enum EnumExample {
-    ACTIVE,
-    NON_ACTIVE,
+    ACTIVE("user currently working on"),
+    NON_ACTIVE ("User currently not working on"),
+    NON_REGISTERED("User never registered");
 
+    private String description;
+
+    EnumExample(String description ){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
