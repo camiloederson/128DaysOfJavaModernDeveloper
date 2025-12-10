90956a93375b6a7929bb45e6e6e371ab661196cb
MikaDev <camilo.ederson@gmail.com>
2025-12-09 18:04:14 -0600

    feat: BufferedReader drill

--- src/main/java/JavaCoreFoundations/day14.java
+++ src/main/java/JavaCoreFoundations/day14.java
@@ -1,6 +1,8 @@
 package JavaCoreFoundations;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +11,22 @@ public class day14 {
     public static void main(String[] args) {
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of("C:\\Users\\kmika\\OneDrive\\Desktop\\MyFirstTxtFile.txt"))) {
             writer.write("Hello.I'm writing my first .txt file!! Go Ahead MikDev!!");
-            ;
+            writer.newLine();
+            writer.write("It was created on my BootCamp day 14");
+            writer.newLine();
+            writer.write("✅ Done!");
         }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try(BufferedReader bufferedReader = Files.newBufferedReader(Path.of("C:\\Users\\kmika\\OneDrive\\Desktop\\MyFirstTxtFile.txt"))){
+            String line;
+            int lineCounter = 1;
+            while((line = bufferedReader.readLine())!= null){
+                System.out.println("Linea " + lineCounter + " | " + line);
+                lineCounter++;
+            }
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
