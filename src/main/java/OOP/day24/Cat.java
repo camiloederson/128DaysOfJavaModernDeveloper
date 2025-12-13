ed3a7a3312028c69e5776e06f7fe4ebf9b82dd62
MikaDev <camilo.ederson@gmail.com>
2025-12-12 22:38:54 -0600

    feat: Polymorphism in Abstract Classes

---
+++ src/main/java/OOP/day24/Cat.java
@@ -0,0 +1,12 @@
+package OOP.day24;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("I'm " + getName() + " and I'm meowing");
+    }
+}
