6a3f9c347d0619ab109115dd8ab3f0ceb171244f
MikaDev <camilo.ederson@gmail.com>
2025-12-19 10:33:03 -0600

    feat: Interface Segregation Principle: fat Interface

---
+++ src/main/java/OOP/day28/MusicalInstrument.java
@@ -0,0 +1,9 @@
+package OOP.day28;
+
+public interface MusicalInstrument {
+    public void makeSound();
+    public boolean isSounding();
+    public int turnUp();
+    public int turnDown();
+    public void tune();
+}
