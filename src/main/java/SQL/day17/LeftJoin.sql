370937b315b0e5803647fa5d25d40911a4b2e35d
MikaDev <camilo.ederson@gmail.com>
2025-12-29 16:26:47 -0600

    feat: Mastering Left Join

--- src/main/java/SQL/day17/LeftJoin.sql
+++ src/main/java/SQL/day17/LeftJoin.sql
@@ -6,3 +6,13 @@ SELECT c.CustomerNAME, o.OrderID
 FROM Customers c LEFT JOIN Orders o
                            ON c.CustomerID = o.CustomerID
 WHERE o.OrderID is NULL
+
+SELECT c.CustomerName as CustomerName, o.OrderID as OrderNumber, o.OrderDate as OrderDate
+FROM Customers c LEFT JOIN Orders o
+                           ON c.CustomerID = o.CustomerID
+WHERE o.OrderDate BETWEEN '1997-01-01' AND '1997-12-31'
+
+SELECT c.CustomerName as CustomerName, COUNT(O.OrderID) as NumberOfOrders
+FROM Customers c LEFT JOIN Orders o
+                           ON c.CustomerID = o.CustomerID
+GROUP BY c.CustomerName
