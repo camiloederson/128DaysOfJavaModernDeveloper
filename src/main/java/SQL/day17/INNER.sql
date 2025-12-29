e44bb4af0fb25be26196f5e89f6c12b481ba099a
MikaDev <camilo.ederson@gmail.com>
2025-12-28 21:05:00 -0600

    feat: Third Drill using InnerJoin with multiple tables

--- src/main/java/SQL/day17/INNER.sql
+++ src/main/java/SQL/day17/INNER.sql
@@ -9,3 +9,12 @@ FROM Orders
          INNER JOIN Customers
                     ON Orders.CustomerID = Customers.CustomerID
 WHERE Customers.Country = 'Germany'
+
+SELECT o.OrderID, c.CustomerName, p.ProductName
+FROM Orders o
+    inner join customers c
+on o.CustomerID = c.CustomerID
+inner join orderdetails od
+    on o.OrderID = od.OrderID
+inner join Products p
+    ON od.ProductID = p.ProductID
