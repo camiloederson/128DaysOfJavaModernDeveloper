1baa469bd94b2be0b16799d044895e4431351497
MikaDev <camilo.ederson@gmail.com>
2025-12-28 20:45:42 -0600

    feat: INNER JOIN adding where conditions

--- src/main/java/SQL/day17/INNER.sql
+++ src/main/java/SQL/day17/INNER.sql
@@ -2,3 +2,10 @@ SELECT Orders.OrderId, Customers.CustomerName
 FROM Orders
          INNER JOIN Customers
                     ON Orders.CustomerID = Customers.CustomerID
+
+-- Using where
+SELECT Orders.CustomerId, Customers.CustomerName
+FROM Orders
+         INNER JOIN Customers
+                    ON Orders.CustomerID = Customers.CustomerID
+WHERE Customers.Country = 'Germany'
