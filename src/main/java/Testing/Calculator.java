aacaff94cbad803cfb5cb1a65f3677e0ad0f8da4
MikaDev <camilo.ederson@gmail.com>
2025-12-30 17:04:57 -0600

    feat: assertEquals andassertThrows

--- src/main/java/Testing/Calculator.java
+++ src/main/java/Testing/Calculator.java
@@ -1,7 +1,20 @@
 package Testing;
 
 public class Calculator {
-    public int addition(int num1, int num2) {
+    public double addition(double num1, double num2) {
         return num1 + num2;
+    }
+
+    public double division(double num1, double num2) {
+        if (num2 == 0) throw new ArithmeticException("Divisor should not be zero");
+        return num1 / num2;
+    }
+
+    public double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double multiplication(double num1, double num2) {
+        return num1 * num2;
     }
 }
--- src/test/java/Testing/CalculatorTest.java
+++ src/test/java/Testing/CalculatorTest.java
@@ -2,17 +2,19 @@ package Testing;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.PrivateKey;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculatorTest {
 
+    private final Calculator calculator = new Calculator();
+
     @Test
     void shouldAddTwoNumbers() {
-        // ARRANGE
-        Calculator calculator = new Calculator();
-
         // ACT
-        int result = calculator.addition(1, 2);
+        double result = calculator.addition(1, 2);
 
         // ASSERT
         assertEquals(3, result);
@@ -20,13 +22,35 @@ class CalculatorTest {
 
     @Test
     void shouldAddTwoNegativeNumbers(){
-        // ARRANGE
-        Calculator calculator = new Calculator();
-
         // ACT
-        int result = calculator.addition(-1, -2);
+        double result = calculator.addition(-1, -2);
 
         // ASSERT
         assertEquals(-3, result);
+    }
+
+    @Test
+    void shouldDivideTwoValidNumbers(){
+        double result = calculator.division(10, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void shouldThrowAnArithmeticException(){
+        ArithmeticException exception =
+                assertThrows(ArithmeticException.class,
+                        ()  -> calculator.division(1, 0));
+    }
+
+    @Test
+    void shouldSubtractTw0Numbers(){
+        double result = calculator.subtraction(10, 5);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void shouldMultiplyTwoNumbers(){
+        double result = calculator.multiplication(10 , 10);
+        assertEquals(100, result);
     }
 };
